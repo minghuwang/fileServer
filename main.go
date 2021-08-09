d5e84c41bdb48a45729b1e10bbd41af27e0ab16b
Richard Wang <rwang@iqhive.com>
2021-08-10 11:46:16 +1200

    change root path

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	port := ":8080"
 	fileServer := http.FileServer(http.Dir("./"))
-	http.Handle("/", fileServer)
+	http.Handle("/voa", fileServer)
 	fmt.Println("Starting server at port", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
